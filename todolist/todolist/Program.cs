f3e44fbc9403d0ca0908f44eb8830a554ec36cde
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2026-01-12 12:58:19 -0800

    Initial file upload from local

---
+++ todolist/todolist/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 10;
+int b = 1;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+Console.WriteLine(++a);
+Console.WriteLine(--b);
+
+Console.WriteLine(a++);
+Console.WriteLine(b--);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+Console.ReadKey();
