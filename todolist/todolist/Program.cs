4f82a6f6cf64d9e79a3594642a7eb35b40b4f4a2
baker6romeo <baker6romeo@gmail.com>
2026-01-14 14:03:06 -0800

    As of lesson 29

--- todolist/todolist/Program.cs
+++ todolist/todolist/Program.cs
@@ -1,16 +1,20 @@
-﻿int a = 10;
-int b = 1;
+﻿Console.WriteLine("Hello, Chum!");
+Console.WriteLine("What would you like to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[E]xit");
 
-Console.WriteLine(a);
-Console.WriteLine(b);
+string? userInput = Console.ReadLine();
+Console.WriteLine($"User Input: {userInput}");
 
-Console.WriteLine(++a);
-Console.WriteLine(--b);
-
-Console.WriteLine(a++);
-Console.WriteLine(b--);
-
-Console.WriteLine(a);
-Console.WriteLine(b);
+string? userChoice = Console.ReadLine();
+if(userChoice.Length < 0)
+{
+  Console.WriteLine($"Non-empty choice: {userChoice}");
+} else
+{
+  Console.WriteLine("Empty choice");
+}
+Console.WriteLine($"Your choice is: {userChoice}");
 
 Console.ReadKey();
